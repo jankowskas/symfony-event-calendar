f10b8a1252c6f2d9cc49405256ee21b1fa73cbeb
Sandra <sandra.tolkacz@gmail.com>
2023-03-03 22:52:48 +0100

    simple api for events

--- app/src/Provider/EventProvider.php
+++ app/src/Provider/EventProvider.php
@@ -3,18 +3,28 @@
 namespace App\Provider;
 
 use App\Repository\EventRepository;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class EventProvider
 {
     private EventRepository $eventRepository;
+    private TagAwareCacheInterface $cache;
 
-    public function __construct(EventRepository $eventRepository)
+    public function __construct(EventRepository $eventRepository, TagAwareCacheInterface $cache)
     {
         $this->eventRepository = $eventRepository;
+        $this->cache = $cache;
     }
 
     public function provide(): array
     {
-        return $this->eventRepository->findAll();
+        $cacheId = 'events';
+
+        return $this->cache->get($cacheId, function (ItemInterface $item) use ($cacheId) {
+            $item->expiresAfter(100);
+            $item->tag($cacheId);
+            return $this->eventRepository->findAll();
+        });
     }
 }

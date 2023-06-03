86a10920ee545f00d8935bf69129865c75369f63
Sandra <sandra.tolkacz@gmail.com>
2023-06-03 23:33:20 +0200

    wip

--- app/src/Enum/DivisionEnum.php
+++ app/src/Enum/DivisionEnum.php
@@ -4,14 +4,28 @@ namespace App\Enum;
 
 class DivisionEnum
 {
-    public const CW = 'CW';
-    public const CM = 'CM';
+    public const COMPOUND_TARGET = 'Compound Target';
+    public const COMPOUND_HUNTING = 'Compound Hunting';
+    public const COMPOUND_BAREBOW = 'Compound Barebow';
+    public const OLYMPIC_RECURVE = 'Olympic Recurve';
+    public const BAREBOW = 'Barebow';
+    public const TRADITIONAL_RECURVE = 'Traditional Recurve';
+    public const BOWHUNTER_RECURVE = 'Bowhunter Recurve';
+    public const LONGBOW = 'Longbow';
+    public const HISTORICAL = 'Historical';
 
     public static function getChoices(): array
     {
         return [
-            self::CW => self::CW,
-            self::CM => self::CM,
+            self::COMPOUND_TARGET => self::COMPOUND_TARGET,
+            self::COMPOUND_HUNTING => self::COMPOUND_HUNTING,
+            self::COMPOUND_BAREBOW => self::COMPOUND_BAREBOW,
+            self::OLYMPIC_RECURVE => self::OLYMPIC_RECURVE,
+            self::BAREBOW => self::BAREBOW,
+            self::TRADITIONAL_RECURVE => self::TRADITIONAL_RECURVE,
+            self::BOWHUNTER_RECURVE => self::BOWHUNTER_RECURVE,
+            self::LONGBOW => self::LONGBOW,
+            self::HISTORICAL => self::HISTORICAL,
         ];
     }
 }
--- app/src/Enum/EventCategoryEnum.php
+++ app/src/Enum/EventCategoryEnum.php
@@ -5,13 +5,15 @@ namespace App\Enum;
 class EventCategoryEnum
 {
     public const WA = 'World Archery';
-    public const IFAA = 'International Field Archery Assiciation';
+    public const IFAA = 'International Field Archery Association';
+    public const CUSTOM = 'Custom';
 
     public static function getChoices(): array
     {
         return [
             self::WA => self::WA,
             self::IFAA => self::IFAA,
+            self::CUSTOM => self::CUSTOM,
         ];
     }
 }

e80abbc9aa18ed6780e489e179f5d01495e3d50b
Sandra <sandra.tolkacz@gmail.com>
2023-06-29 00:10:30 +0200

    wip

--- app/src/Form/FiltersType.php
+++ app/src/Form/FiltersType.php
@@ -2,12 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Association;
 use App\Enum\AssociationsEnum;
 use App\Enum\BowTypesEnum;
 use App\Enum\ClassesEnum;
 use App\Enum\DivisionsEnum;
 use App\Enum\RoundsEnum;
+use App\Repository\AssociationRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -16,8 +19,25 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FiltersType extends AbstractType
 {
+    private AssociationRepository $associationRepository;
+
+    public function __construct(AssociationRepository $associationRepository)
+    {
+        $this->associationRepository = $associationRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $associations = $this->associationRepository->findAll();
+
+        $associationsChoices = [];
+
+        foreach ($associations as $association) {
+            $associationsChoices[$association->getName()] = $association->getId();
+        }
+
+        dump($associationsChoices);
+
         $builder
             ->add('search', TextareaType::class, [
                 'label' => 'Szukaj',
@@ -26,50 +46,11 @@ class FiltersType extends AbstractType
                     'placeholder' => 'Nazwa wydarzenia',
                 ],
             ])
-            ->add('associations', EnumType::class, [
-                'class' => AssociationsEnum::class,
+            ->add('associations', ChoiceType::class, [
                 'expanded' => true,
                 'required' => false,
                 'multiple' => true,
-                'choice_label' => function (?AssociationsEnum $enum) {
-                    return $enum->value;
-                }
-            ])
-            ->add('bowTypes', EnumType::class, [
-                'class' => BowTypesEnum::class,
-                'expanded' => true,
-                'required' => false,
-                'multiple' => true,
-                'choice_label' => function (?BowTypesEnum $enum) {
-                    return $enum->value;
-                }
-            ])
-            ->add('classes', EnumType::class, [
-                'class' => ClassesEnum::class,
-                'expanded' => true,
-                'required' => false,
-                'multiple' => true,
-                'choice_label' => function (?ClassesEnum $enum) {
-                    return $enum->value;
-                }
-            ])
-            ->add('divisions', EnumType::class, [
-                'class' => DivisionsEnum::class,
-                'expanded' => true,
-                'required' => false,
-                'multiple' => true,
-                'choice_label' => function (?DivisionsEnum $enum) {
-                    return $enum->value;
-                }
-            ])
-            ->add('rounds', EnumType::class, [
-                'class' => RoundsEnum::class,
-                'expanded' => true,
-                'required' => false,
-                'multiple' => true,
-                'choice_label' => function (?RoundsEnum $enum) {
-                    return $enum->value;
-                }
+                'choices' => $associationsChoices,
             ])
             ->add('submit', SubmitType::class)
         ;
